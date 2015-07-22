edd2123800279dbf3a1e0b6fc4a846acb170ac33
GRACHYA <grach41@gmail.com>
2015-07-23 01:27:24 +0300

    ass main text and order form

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
-	// $('#accordionSideBar').accordion({
-	// 	collapsible: true
-	// })
+
 
 //sideBarSlideDown
 	$('#li2').on('click', function(){
 		$('.aboutMethod').slideToggle();
 	})
-
 //datapicker
-	// $.datepicker.setDefaults(
-	// 	$.extend($.datepicker.regional["ru"]);
-	// );
 	$.datepicker.setDefaults( $.datepicker.regional[ "ru" ] );
  	$( "#datepicker" ).datepicker({
  		autoSize: true
  	});	
-})
+//slicker slider
+	$('.sidebarSlider').slick({
+		appendArrows:$('.sidebarSlider')
+	});
+	$('.maincontentSlider').slick({
+		appendArrows:$('.maincontentSlider')
+	})
+
+
+});
